1a3ef098d7f0bcfbedfdb12005f822086e6d0935
YuchaoZhengNYIT <yzheng20@nyit.edu>
2021-03-04 21:44:40 +0800

    add try catch and fix error

--- Assign_2.java
+++ Assign_2.java
@@ -6,18 +6,26 @@
 class Assign_2 { 
     public static void main(String[] args) 
     { 
-    	String message = args[0];
-    	String input_2 = args[1];
-    	String[] input_2s = input_2.split(",",2);
-        String depth = input_2s[0].substring(1,input_2s[0].length());
-        String rtimes = input_2s[1].substring(0,input_2s[0].length()-1);
+    	try {
+	    	String message = args[0];
+	    	String input_2 = args[1];
+	    	String[] input_2s = input_2.split(",",2);
+	        String depth = input_2s[0].substring(1,input_2s[0].length());
+	        String rtimes = input_2s[1].substring(0,input_2s[1].length()-1);
 
-        Integer depth_i = Integer.parseInt(depth);
-        Integer rtimes_i = Integer.parseInt(rtimes);
+	        Integer depth_i = Integer.parseInt(depth);
+	        Integer rtimes_i = Integer.parseInt(rtimes);
 
-        System.out.println("message is "+message); 
-        System.out.println("depth is "+depth_i);
-        System.out.println("rtimes is "+rtimes_i);
+	        System.out.println("message is "+message); 
+	        System.out.println("depth is "+depth_i);
+	        System.out.println("rtimes is "+rtimes_i);
+	    }
+	    catch(Exception e){
+	    	System.out.println("Please input the correct format."); 
+	    	System.out.println("example: Assign_2 message [4,23] "); 
+	    }
+
+	    
 
     } 
 }
